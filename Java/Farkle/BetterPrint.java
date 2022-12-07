4171df1345752295ca010db20abbe2e65e2c22fa
HoodiedLynx <118916763+HoodiedLynx@users.noreply.github.com>
2022-12-07 13:04:00 -0500

    Create BetterPrint.java

---
+++ Java/Farkle/BetterPrint.java
@@ -0,0 +1,45 @@
+package Java.BetterPrint;
+
+public class BetterPrint{
+  public void printStr(String input)
+  {
+    System.out.print(input);
+  }
+  public void printInt(int input)
+  {
+    System.out.print(input);
+  }
+  public void printDoub(double input)
+  {
+    System.out.print(input);
+  }
+  public void printBool(boolean input)
+  {
+    System.out.print(input);
+  }
+  public void printByte(byte input)
+  {
+    System.out.print(input);
+  }
+  public void printShort(short input)
+  {
+    System.out.print(input);
+  }
+  public void printLong(long input)
+  {
+    System.out.print(input);
+  }
+  public void space()
+  {
+    System.out.print(" ");
+  }
+  public void tab()
+  {
+    space();
+    space();
+  }
+  public void enter()
+  {
+    System.out.println();
+  }
+}
